188303213a47fdcafce5cd0f33868cffa20dc0af
Vlad Turcanu <vlad.turcanu95@gmail.com>
2019-01-13 19:15:23 +0200

    User can edit his own data now

--- account.php
+++ account.php
@@ -86,8 +86,51 @@
             } else {
                 $res['type'] = "<span class='label bg-light-blue'>Volunteer</span>";
             }
+
+            if (isset($res['city'])) {
+                $city = $res['city'];
+            } else {
+                $city = "";
+            }
         }
     ?>
+
+    <div id="edit-account-details-modal" class="modal" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Add incident</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p id="modal-err" class="error-msg hidden"></p>
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" id="name" placeholder="Name" value="<?= $res['name'] ?>">
+                </div>
+                <div class="form-group">
+                    <label for="surname">Surname</label>
+                    <input type="text" class="form-control" id="surname" placeholder="Surame" value="<?= $res['surname'] ?>">
+                </div>
+                <div class="form-group">
+                    <label for="city">City</label>
+                    <input type="text" class="form-control" id="city" placeholder="Bucuresti" value="<?= $city ?>">
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="text" class="form-control" id="email" placeholder="user@example.com" value="<?= $res['email'] ?>">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary" onclick="editAccountDetails()">Save</button>
+            </div>
+            </div>
+        </div>
+    </div>
+
     <div class="container">
         <div class="account-details">
             <h2 class="page-title">My Account</h2>
@@ -101,19 +144,19 @@
                             <tbody>
                                 <tr>
                                     <td><strong>Name:</strong></td>
-                                    <td><?= $res["name"] ?></td>
+                                    <td id="user_name"><?= $res["name"] ?></td>
                                 </tr>
                                 <tr>
-                                    <td><strong>Surame:</strong></td>
-                                    <td><?= $res["surname"] ?></td>
+                                    <td><strong>Surname:</strong></td>
+                                    <td id="user_surname"><?= $res["surname"] ?></td>
                                 </tr>
                                 <tr>
                                     <td><strong>City:</strong></td>
-                                    <td><?= $res["city"] ?></td>
+                                    <td id="user_city"><?= $res["city"] ?></td>
                                 </tr>
                                 <tr>
                                     <td><strong>Email:</strong></td>
-                                    <td><?= $res["email"] ?></td>
+                                    <td id="user_email"><?= $res["email"] ?></td>
                                 </tr>
                                 <tr>
                                     <td><strong>Account type:</strong></td>
@@ -129,9 +172,84 @@
                 </div>
             <?php endif; ?>
             <a href="account.php?logout" class="btn btn-primary mt-2">Logout</a>
-            <a href="account.php?logout" class="btn btn-info mt-2 ml-2">Edit details</a>
+            <a href="#" class="btn btn-info mt-2 ml-2" data-toggle="modal" data-target="#edit-account-details-modal">Edit details</a>
         </div>
     </div>
+
+    <script>
+        function editAccountDetails() {
+            var name = $("#name").val();
+            var surname = $("#surname").val();
+            var city = $("#city").val();
+            var email = $("#email").val();
+
+            if (!name || !surname || !email) {
+                $("#modal-err").removeClass("hidden");
+                $("#modal-err").html("Name, surname and email are required.");
+                return;
+            }
+
+            $("#modal-err").addClass("hidden");
+
+            var postData = JSON.stringify({
+                "name": name,
+                "surname": surname,
+                "city": city,
+                "email": email,
+                "token": "<?= $_SESSION['token'] ?>"
+            });
+
+            $.post("https://serene-cove-78266.herokuapp.com/edit_user_data", postData)
+                .done(function(data) {
+                    if (data["error"]) {
+                        $("#modal-err").removeClass("hidden");
+                        $("#modal-err").html(data["error"]);
+                        return;
+                    } else {
+                        $("#modal-err").addClass("hidden");
+                        $("#edit-account-details-modal").modal('hide');
+                        populateUserData();
+                    }
+                })
+                .fail(function(data) {
+                    if (data["error"]) {
+                        $("#modal-err").removeClass("hidden");
+                        $("#modal-err").html(data["error"]);
+                        return;
+                    } else {
+                        $("#modal-err").removeClass("hidden");
+                        $("#modal-err").html("Error communicating to the server");
+                        return;
+                    }
+                });
+        }
+
+        function populateUserData() {
+
+            var postData = JSON.stringify({
+                "token": "<?= $_SESSION['token'] ?>",
+                "username": "<?= $_SESSION['username'] ?>"
+            });
+
+            /* Get user data from server */
+            $.post("https://serene-cove-78266.herokuapp.com/get_user_data", postData)
+                .done(function(data) {
+                    if (data["error"]) {
+                        alert(data["error"]);
+                        return;
+                    } else {
+                        $("#user_name").html(data["name"]);
+                        $("#user_surname").html(data["surname"]);
+                        $("#user_city").html(data["city"]);
+                        $("#user_email").html(data["email"]);
+                    }
+                })
+                .fail(function () {
+                    alert("Connection to the server could not be established. Please refresh the page.");
+                    return;
+                });
+        }
+    </script>
 <?php else: ?>
     <div class="container">
         <div class="center-content text-center">
